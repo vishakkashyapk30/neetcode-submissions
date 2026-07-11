4baf701fa77ac731d2d4a96a33ac6bb639d258f2
Vishak Kashyap K <143735113+vishakkashyapk30@users.noreply.github.com>
2026-07-12 03:32:30 +0530

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.cpp
@@ -0,0 +1,62 @@
+class MyHashMap 
+{
+    private:
+        static const int SIZE = 1001;
+        vector<vector<pair<int, int>>> table;
+        int hash(int key)
+        {
+            return key%SIZE;
+        }
+
+    public:
+        MyHashMap() 
+        {
+            table.resize(SIZE);
+        }
+        
+        void put(int key, int value) 
+        {
+            int idx=hash(key);
+            for(auto &p: table[idx])
+            {
+                if(p.first == key)
+                {
+                    p.second=value;
+                    return;
+                }
+            }
+            table[idx].push_back({key, value});
+        }
+        
+        int get(int key) 
+        {
+            int idx=hash(key);
+            for(auto &p: table[idx])
+            {
+                if(p.first == key)
+                    return p.second;
+            }
+            return -1;
+        }
+        
+        void remove(int key) 
+        {
+            int idx=hash(key);
+            for(auto it = table[idx].begin(); it!=table[idx].end(); it++)
+            {
+                if(it->first == key)
+                {
+                    table[idx].erase(it);
+                    return;
+                }
+            }
+        }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
